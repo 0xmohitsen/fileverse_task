c963656efdc7ed981cc3de8bb75e808abf74f7ac
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-21 15:46:31 +0530

    task: Parsed all the links of the specified webpage + saved the results in json format

---
+++ README.md
@@ -0,0 +1,30 @@
+# Webpage Anchor Tag Scraper
+
+This Node.js script scrapes anchor tags (links) from a webpage and extracts their titles and href attributes using the x-ray package.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Options](#options)
+- [License](#license)
+
+## Introduction
+
+This script allows users to scrape anchor tags (links) from a webpage and extract their titles and href attributes. It provides a simple command-line interface (CLI) for specifying the URL of the webpage to be scraped.
+
+## Prerequisites
+
+Before using this script, ensure you have the following prerequisites installed:
+
+- Node.js: [Download and install Node.js](https://nodejs.org/)
+- npm (Node Package Manager): Included with Node.js installation
+
+## Installation
+
+To install the required dependencies, run the following command in your terminal:
+
+```bash
+npm install
---
+++ index.js
@@ -0,0 +1,46 @@
+const Xray = require("x-ray"); // Imported Xray package
+
+const fs = require("fs"); // Imported file system ( fs ) package
+const argv = require("yargs").argv; // Imported yargs to specify url via CLI
+
+const xray = new Xray(); // Created x-ray object and stored in xray variable
+
+const url = argv.url || ""; // Get url from CLI argument
+
+// Check whether url is given or not
+if (!url) {
+  console.error("Please specify a URL");
+  process.exit(1);
+}
+
+xray(url, "a", [
+  {
+    Title: "", // Extract the text content of the anchor tag
+    Links: "@href", // Extract the href attribute of the anchor tag
+  },
+])(function (err, links) {
+  if (err) {
+    console.error("Error:", err);
+    return;
+  }
+
+  // Remove duplicates
+  const uniqueLinks = links.filter(
+    (link, index, self) =>
+      index ===
+      self.findIndex((l) => l.Title === link.Title && l.Href === link.Href)
+  );
+
+  // Write the results to a JSON file
+  fs.writeFile(
+    "results.json",
+    JSON.stringify(uniqueLinks, null, 2),
+    function (err) {
+      if (err) {
+        console.error("Error writing to file:", err);
+        return;
+      }
+      console.log("Scraping completed. Results saved to results.json");
+    }
+  );
+});
